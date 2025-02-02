ecb6938ec68a41e79398dc0dd9b9ce7b47a957c5
onicolasmendes <nicolasmela312@gmail.com>
2025-02-02 13:03:52 -0300

    pagerank project 2

--- project2/pagerank/pagerank.py
+++ project2/pagerank/pagerank.py
@@ -57,7 +57,26 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+    # Novo dicionário
+    probabilities = {key: 0.0 for key in corpus}
+    numPages = len(probabilities)
+    
+    # Verificando se page tem links de saída
+    if len(corpus[page]) == 0:
+        for key in corpus:
+            # Mesma probabilidade para todas páginas
+            probabilities[key] = 1.0/numPages
+    # Caso o page tenha um ou mais links
+    else:
+        # Probabilidade = Damping Factor
+        for link in corpus[page]:
+            probabilities[link] = damping_factor / len(corpus[page])
+        
+        # Probabilidade =  1 - Damping Factor
+        for link in probabilities:
+            probabilities[link] += (1 - damping_factor) / len(probabilities)
+    
+    return probabilities
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -69,9 +88,29 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
-
-
+    # Novo dicionário
+    sampleRank = {key: 0.0 for key in corpus}
+    # Primeira página escolhida aleatoriamente
+    chosenPage = random.choice(list(sampleRank.keys()))
+    sampleRank[chosenPage] += 1.0
+    
+    for _ in range(n-1):
+        # Gera as probabilidades de escolhe cada página, dada a página atual
+        probabilities = transition_model(corpus, chosenPage, damping_factor)
+        
+        # Escolhe a próxima página atual, baseada nas probabilidades do modelo de transição da amostra anterior
+        chosenPage = random.choices(list(probabilities.keys()), weights=probabilities.values())[0]
+        
+        # Aumenta a contagem de vezes em que a pagina foi escolhida
+        sampleRank[chosenPage] += 1.0
+    
+    # Calcula a proporção de amostras que correspondem à cada página
+    for page in sampleRank:
+        sampleRank[page] = sampleRank[page] / n
+    
+    return sampleRank
+  
+      
 def iterate_pagerank(corpus, damping_factor):
     """
     Return PageRank values for each page by iteratively updating
@@ -81,8 +120,47 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
-
-
+    # Novo dicionário
+    iterateRank = {key: 0.0 for key in corpus}
+    # Variável de controle para continuar o cálculo iterativo
+    keepUpdate = True
+    
+    # Classificação inicial = 1 / N
+    for page in iterateRank:
+        iterateRank[page] = 1.0 / len(iterateRank)
+    
+    # Dicionário para manter valores da iteração anterior para verificar a necessidade de continuar os cálculos
+    oldValues = {key: 0.0 for key in iterateRank}
+    
+    # Calcular classificação de todas as páginas page que fazem link para página pr
+    while (keepUpdate):
+        for pr in iterateRank:
+            sum = 0.0
+            # Contabiliza-se as páginas que tem link para página pr
+            for page in corpus:
+                # Caso a page não tenha links, considera-se links para todas as páginas, inclusive ela mesma
+                if not corpus[page]:
+                    numLinks = len(corpus)  
+                    # Somatório
+                    sum += iterateRank[page] / numLinks
+                # Caso a página page tenha link para a página pr, contabiliza-se todos os links que page possui      
+                if pr in corpus[page]:
+                    numLinks = len(corpus[page])
+                    # Somatório
+                    sum += iterateRank[page] / numLinks
+              
+            # Atualiza o pageRank
+            oldValues[pr] = iterateRank[pr]
+            iterateRank[pr] = ((1 - damping_factor) / len(corpus)) + damping_factor * sum
+        # Verifica se deve continuar efetuando cálculos
+        keepUpdate = False
+        for page in oldValues:
+            # Caso algum valor seja alterado em um um valor maior que 0.001, deve se calcular mais uma iteração
+            if (abs(oldValues[page] - iterateRank[page]) > 0.001):
+                keepUpdate = True
+                
+    return iterateRank
+                
+                         
 if __name__ == "__main__":
     main()
